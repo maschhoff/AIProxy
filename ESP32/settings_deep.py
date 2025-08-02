f34ddcf0da1a00aa49eb99a132f4935e335eb5e3
Mathias Aschhoff <4007829+maschhoff@users.noreply.github.com>
2025-08-02 09:14:27 +0200

    Update settings_deep.py

--- ESP32/settings_deep.py
+++ ESP32/settings_deep.py
@@ -1,7 +1,8 @@
+# Settings 
+
 SSID = "Dein-WLAN"
 PASSWORD = "Dein-Passwort"
 DEEPAI_API_KEY = "dein_deepai_api_schlüssel"
-
 MQTT_BROKER = "192.168.1.100"  # IP deines MQTT Brokers
 MQTT_PORT = 1883
 MQTT_TOPIC = "zaehlerstand/Strom"
