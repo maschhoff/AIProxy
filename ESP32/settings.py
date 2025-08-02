4ecd1756587adbdd77ca1e4fa4c3fbd539cbdc77
Mathias Aschhoff <4007829+maschhoff@users.noreply.github.com>
2025-08-02 21:00:26 +0200

    Create settings.py

---
+++ ESP32/settings.py
@@ -0,0 +1,9 @@
+# AIProxy ESP32 CAM Image Module
+# Mathias Aschhoff 2025
+
+SSID = "Your SSID"
+PASSWORD = "Your Secret"
+API_KEY = "Gimini API Key"
+MQTT_BROKER = "192.168.100.60"  # IP deines MQTT Brokers
+MQTT_PORT = 1883
+MQTT_TOPIC = "zaehlerstand/Strom"
