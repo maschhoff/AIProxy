3b4222d7f0386352cb279199377de9904d343599
Mathias Aschhoff <4007829+maschhoff@users.noreply.github.com>
2025-08-05 22:30:10 +0200

    Update settings.py

--- ESP32/settings.py
+++ ESP32/settings.py
@@ -3,7 +3,8 @@
 
 SSID = "Your SSID"
 PASSWORD = "Your Secret"
-API_KEY = "Gimini API Key"
-MQTT_BROKER = "192.168.100.60"  # IP deines MQTT Brokers
+API_KEY = "Gimini_API_KEY"
+MQTT_BROKER = "192.168.100.60"  
 MQTT_PORT = 1883
-MQTT_TOPIC = "zaehlerstand/Strom"
+MQTT_TOPIC = "zaehlerstand/Strom" 
+DEBUG=true # sendet das Bild per MQTT mit.
