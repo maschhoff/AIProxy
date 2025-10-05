12bff320719193091ca0610537f76e4e363f5c4a
Mathias Aschhoff <4007829+maschhoff@users.noreply.github.com>
2025-10-05 18:36:05 +0200

    Update espcams3openai.py

--- ESP32/espcams3openai.py
+++ ESP32/espcams3openai.py
@@ -1,5 +1,5 @@
 # ESP32 S3 CAM Image Module
-# mpy_cam-v1.25.0-FREENOVE_ESP32S3_CAM.zip -> https://github.com/cnadler86/micropython-camera-API/releases
+# mpy_cam-v1.25.0-FREENOVE_ESP32S3_CAM.zip -> https://github.com/maschhoff/micropython-camera-API
 
 import neopixel
 import network
